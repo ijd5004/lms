00ec75d9528e9011e2a6bf7c3d5d14246ddd5333
Ian Davis <ian.davis@gse_laptop>
2018-01-09 16:25:03 +0900

    completed code for calculating the general form of the sum of the squares equation

--- lms.py
+++ lms.py
@@ -28,16 +28,41 @@ def lms(data):
 
     #(Test 2) Display the overdetermined linear system
     #General form equation: beta1 + x_i*beta2 = y_i
-    inp_x = np.empty(len(data))
-    inp_y = np.empty(len(data))
+    nsets = len(data)   #number of points in data
+    inp_x = np.empty(nsets)
+    inp_y = np.empty(nsets)
     print '\n'
     print 'overdetermined linear system:'
-    for i in range(len(data)):
+    for i in range(nsets):
         inp_x[i] = data[i][0]
         inp_y[i] = data[i][1]
         print 'beta1 +',inp_x[i],'* beta2 = ',inp_y[i]
 
     #(Test 3) Display the "sum of the squares" function
+    #General form of the equation:
+    # S(beta1,beta2) =  y^2             (term1)
+    #                 + beta1^2         (term2)
+    #                 + x^2*beta2^2     (term3)
+    #                 - 2*y*beta1       (term4)
+    #                 - 2*y*x*beta2     (term5)
+    #                 + 2*x*beta1*beta2 (term6)
+    terms = np.array([0.0,0.0,0.0,0.0,0.0,0.0])
+    for i in range(nsets):
+        terms[0] = terms[0] + inp_y[i]*inp_y[i]
+        terms[1] = terms[1] + 1.0
+        terms[2] = terms[2] + inp_x[i]*inp_x[i]
+        terms[3] = terms[3] - 2.0*inp_y[i]
+        terms[4] = terms[4] - 2.0*inp_y[i]*inp_x[i]
+        terms[5] = terms[5] + 2.0*inp_x[i]
+    print '\n'
+    print 'Sum of the squares:'
+    print 'S(beta1,beta2) = '+str(terms[0])+' + ' \
+                            +str(terms[1])+'*beta1^2 + ' \
+                            +str(terms[2])+'*beta2^2 + ' \
+                            +str(terms[3])+'*beta1 + ' \
+                            +str(terms[4])+'*beta2 + ' \
+                            +str(terms[5])+'*beta1*beta2'
+
 
 
     #(Test 4) Display the partial derivative equations for the coeff.
